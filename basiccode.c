9afe925f0335a05d46288715b215a9ed42d45ec8
Tarun yadav <48976546+tariqaat@users.noreply.github.com>
2019-03-28 10:59:31 +0530

    Update basiccode.c

--- basiccode.c
+++ basiccode.c
@@ -1,27 +1,8 @@
-#include <stdio.h>
-
-int main()
+int max(int a, int b)
 {
-    int year;
-
-    printf("Enter a year: ");
-    scanf("%d",&year);
-
-    if(year%4 == 0)
-    {
-        if( year%100 == 0)
-        {
-            // year is divisible by 400, hence the year is a leap year
-            if ( year%400 == 0)
-                printf("%d is a leap year.", year);
-            else
-                printf("%d is not a leap year.", year);
-        }
-        else
-            printf("%d is a leap year.", year );
-    }
-    else
-        printf("%d is not a leap year.", year);
-    
-    return 0;
+ if (a>b)
+ {
+ return a;
+ }
+ return b;
 }
